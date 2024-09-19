c46fdcca1ddc47c1be929be4e04d5f44337c0a40
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2024-09-19 15:39:29 +0000

    pattern_1

--- pattern_1.java
+++ pattern_1.java
@@ -5,3 +5,15 @@
 * * * * *
 * * * * *
   */
+
+ public class pattern_1 { 
+  public static void main(String[] args){
+    int i,j,n=5;
+    for(i=1 ;i<=n;i++){
+      for(j=1;j<=n;j++ ){
+      System.out.print("*");
+      }
+      System.out.println(" ");
+    }
+  }
+ }
