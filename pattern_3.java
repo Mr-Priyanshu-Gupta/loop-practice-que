35c556db667c46e4d28b09fc913db9e0c8e79621
Mr-Priyanshu-Gupta <priyanshuguptaofficial555@gmail.com>
2024-09-19 16:43:16 +0000

    patter_2_3

---
+++ pattern_3.java
@@ -0,0 +1,23 @@
+/*
+
+pattern_3
+*****
+****
+***
+**
+*
+
+ */
+public class pattern_3 {
+
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=5;i>=1;i--){
+            for(j=1;j<=i;j++){
+
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
